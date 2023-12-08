1aab501f2bc4f385b6a14fed298da70529135db3
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-12-08 11:54:54 -0800

    more template changes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/selectionSort/SelectionApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/selectionSort/SelectionApiController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/insertion")  // all requests in file begin with this URI
+@RequestMapping("/api/selection")  // all requests in file begin with this URI
 public class SelectionApiController {
 
     // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
