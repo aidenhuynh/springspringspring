a544167983a64120e0d8d019c19d261f66e3687f
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-12-08 11:52:56 -0800

    created selection sort outline

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/selectionSort/SelectionApiController.java
@@ -0,0 +1,54 @@
+package com.nighthawk.spring_portfolio.mvc.selectionSort;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController // annotation to simplify the creation of RESTful web services
+@RequestMapping("/api/insertion")  // all requests in file begin with this URI
+public class SelectionApiController {
+
+    // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
+    @Autowired
+    private SelectionJpaRepository repository;
+
+    /* GET List of Jokes
+     * @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Selection>> getSorts() {
+        // ResponseEntity returns List of Jokes provide by JPA findAll()
+        return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/while/time")
+    public ResponseEntity<Selection> whileTime(@RequestBody ArrayList<Integer> list) {
+
+        WhileSort whileSort = new WhileSort();
+
+        whileSort.runSort(list);
+
+        repository.save(whileSort);
+
+        return new ResponseEntity<>(whileSort, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+    }
+
+    @PostMapping("/while")
+    public ResponseEntity<Selection> whileSort(@RequestBody ArrayList<Integer> list) {
+
+        WhileSort whileSort = new WhileSort();
+
+        whileSort.runSort(list);
+
+        repository.save(whileSort);
+
+        return new ResponseEntity<>(whileSort, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+    }
+
+}
